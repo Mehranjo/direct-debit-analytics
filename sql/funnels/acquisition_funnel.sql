36de52d3ab0e555e0d8e4d0ffc5e25327a7244a7
Mehran Johargir <125144363+Mehranjo@users.noreply.github.com>
2026-06-22 08:00:15 +0330

    1.1

---
+++ sql/funnels/acquisition_funnel.sql
@@ -0,0 +1,14 @@
+/*
+Acquisition Funnel
+
+Measures conversion from OAuth initiation
+to first successful debit.
+
+Stages:
+A1 OAuth Requested
+A2 OAuth Generated
+A3 OAuth Completed
+A4 Token Received
+A5 Merchant Activated
+A6 First Debit
+*/
---
+++ sql/funnels/activation_funnel.sql
@@ -0,0 +1,12 @@
+/*
+Activation Funnel
+
+Measures whether newly created mandates
+become operational.
+
+Stages:
+Created Mandate
+First Debit
+Second Debit
+Third Debit
+*/
